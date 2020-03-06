93bd8e3bc07f82de1c6c3fa50116440eec456ed9
Andres Arias <leonardoandres.arias09@gmail.com>
2020-03-05 21:45:33 -0500

    Diagramas estructural y funcional para Memento, Comando, Mediador

---
+++ Memento/Ejercicio/src/memento/DCClient.java
@@ -0,0 +1,22 @@
+package memento;
+public class DCClient {
+
+  public static void main(String[] args) {
+    MementoHandler objMementoHandler = new MementoHandler();
+    DataConverter objConverter = new DataConverter();
+
+    objConverter.setMemento(objMementoHandler.getMemento());
+
+    if (!(objConverter.process())) {
+      System.out.println("Description: Invalid data - " +
+                         "Process Stopped");
+      System.out.println("Please correct the Data and " +
+                         "Run the Application Again");
+      objMementoHandler.setMemento(
+        objConverter.createMemento());
+    }
+    else {
+    	System.out.println("Success");
+    }
+  }
+}
