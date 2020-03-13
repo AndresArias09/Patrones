ed9aee95861502a92f7ea512cd6eff3502d8fd81
Andres Arias <leonardoandres.arias09@gmail.com>
2020-03-12 19:20:44 -0500

    Modelos State

---
+++ Interpreter/Ejercicio/src/interpreter/TerminalExpression.java
@@ -0,0 +1,10 @@
+package interpreter;
+public class TerminalExpression implements Expression {
+  private String var;
+  public TerminalExpression(String v) {
+    var = v;
+  }
+  public int evaluate(Context c) {
+    return c.getValue(var);
+  }
+}
